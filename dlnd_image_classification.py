7dea40f389257c340ce545a8ecf7e8acf3663b3e
hongyang01613 <hongyang01613@163.com>
2018-01-18 20:04:43 +0800

    second commit

--- dlnd_image_classification.py
+++ dlnd_image_classification.py
@@ -11,7 +11,7 @@
 # 请运行以下单元，以下载 [CIFAR-10 数据集（Python版）](https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz)。
 # 
 
-# In[3]:
+# In[1]:
 
 
 """
@@ -76,7 +76,7 @@ tests.test_folder_path(cifar10_dataset_folder_path)
 # 问问你自己：“可能的标签有哪些？”、“图片数据的值范围是多少？”、“标签是按顺序排列，还是随机排列的？”。思考类似的问题，有助于你预处理数据，并使预测结果更准确。
 # 
 
-# In[4]:
+# In[2]:
 
 
 get_ipython().magic('matplotlib inline')
@@ -98,7 +98,7 @@ helper.display_stats(cifar10_dataset_folder_path, batch_id, sample_id)
 # 在下面的单元中，实现 `normalize` 函数，传入图片数据 `x`，并返回标准化 Numpy 数组。值应该在 0 到 1 的范围内（含 0 和 1）。返回对象应该和 `x` 的形状一样。
 # 
 
-# In[5]:
+# In[3]:
 
 
 def normalize(x):
@@ -108,7 +108,7 @@ def normalize(x):
     : return: Numpy array of normalize data
     """
     # TODO: Implement Function
-    return np.array((x / 255), dtype = 'float32')
+    return np.array(((x - np.min(x)) / (np.max(x) - np.min(x))), dtype = 'float32')
 
 
 """
@@ -124,7 +124,7 @@ tests.test_normalize(normalize)
 # 提示：不要重复发明轮子。
 # 
 
-# In[6]:
+# In[4]:
 
 
 def one_hot_encode(x):
@@ -158,7 +158,7 @@ tests.test_one_hot_encode(one_hot_encode)
 # 运行下方的代码单元，将预处理所有 CIFAR-10 数据，并保存到文件中。下面的代码还使用了 10% 的训练数据，用来验证。
 # 
 
-# In[7]:
+# In[5]:
 
 
 """
@@ -173,7 +173,7 @@ helper.preprocess_and_save_data(cifar10_dataset_folder_path, normalize, one_hot_
 # 这是你的第一个检查点。如果你什么时候决定再回到该记事本，或需要重新启动该记事本，你可以从这里开始。预处理的数据已保存到本地。
 # 
 
-# In[8]:
+# In[6]:
 
 
 """
@@ -218,7 +218,7 @@ valid_features, valid_labels = pickle.load(open('preprocess_validation.p', mode=
 # 
 # 注意：TensorFlow 中的 `None` 表示形状可以是动态大小。
 
-# In[9]:
+# In[7]:
 
 
 import tensorflow as tf
@@ -276,7 +276,7 @@ tests.test_nn_keep_prob_inputs(neural_net_keep_prob_input)
 # **注意**：对于**此层**，**请勿使用** [TensorFlow Layers](https://www.tensorflow.org/api_docs/python/tf/layers) 或 [TensorFlow Layers (contrib)](https://www.tensorflow.org/api_guides/python/contrib.layers)，但是仍然可以使用 TensorFlow 的 [Neural Network](https://www.tensorflow.org/api_docs/python/tf/nn) 包。对于所有**其他层**，你依然可以使用快捷方法。
 # 
 
-# In[10]:
+# In[8]:
 
 
 def conv2d_maxpool(x_tensor, conv_num_outputs, conv_ksize, conv_strides, pool_ksize, pool_strides):
@@ -291,18 +291,19 @@ def conv2d_maxpool(x_tensor, conv_num_outputs, conv_ksize, conv_strides, pool_ks
     : return: A tensor that represents convolution and max pooling of x_tensor
     """
     # TODO: Implement Function
-    filter_weights = tf.Variable(tf.truncated_normal((conv_ksize[0], conv_ksize[1], x_tensor.get_shape().as_list()[3], conv_num_outputs), stddev=0.6)) # (height, width, input_depth, output_depth)
+    filter_weights = tf.Variable(tf.truncated_normal((conv_ksize[0], conv_ksize[1], x_tensor.get_shape().as_list()[3], conv_num_outputs), stddev=0.05)) # (height, width, input_depth, output_depth)
     filter_bias = tf.Variable(tf.zeros(conv_num_outputs))
     strides = [1, conv_strides[0], conv_strides[1], 1] # (batch, height, width, depth)
     padding = 'VALID'
     
-    conv = tf.nn.conv2d(x_tensor, filter_weights, strides, padding) + filter_bias
-    conv = tf.nn.relu(conv)
-    
+    conv_layer = tf.nn.conv2d(x_tensor, filter_weights, strides, padding) + filter_bias
+        
     pool_ksize = [1, pool_ksize[0], pool_ksize[1], 1]
     pool_strides = [1, pool_strides[0], pool_strides[1], 1]
-        
-    return tf.nn.max_pool(conv, pool_ksize, pool_strides, padding)
+    
+    max_pool_layer = tf.nn.max_pool(conv_layer, pool_ksize, pool_strides, padding)
+    max_pool_layer = tf.nn.relu(max_pool_layer)
+    return max_pool_layer
 
 """
 DON'T MODIFY ANYTHING IN THIS CELL THAT IS BELOW THIS LINE
@@ -315,7 +316,7 @@ tests.test_con_pool(conv2d_maxpool)
 # 实现 `flatten` 函数，将 `x_tensor` 的维度从四维张量（4-D tensor）变成二维张量。输出应该是形状（*部分大小（Batch Size）*，*扁平化图片大小（Flattened Image Size）*）。快捷方法：对于此层，你可以使用 [TensorFlow Layers](https://www.tensorflow.org/api_docs/python/tf/layers) 或 [TensorFlow Layers (contrib)](https://www.tensorflow.org/api_guides/python/contrib.layers) 包中的类。如果你想要更大挑战，可以仅使用其他 TensorFlow 程序包。
 # 
 
-# In[11]:
+# In[9]:
 
 
 def flatten(x_tensor):
@@ -338,7 +339,7 @@ tests.test_flatten(flatten)
 # 
 # 实现 `fully_conn` 函数，以向 `x_tensor` 应用完全连接的层级，形状为（*部分大小（Batch Size）*，*num_outputs*）。快捷方法：对于此层，你可以使用 [TensorFlow Layers](https://www.tensorflow.org/api_docs/python/tf/layers) 或 [TensorFlow Layers (contrib)](https://www.tensorflow.org/api_guides/python/contrib.layers) 包中的类。如果你想要更大挑战，可以仅使用其他 TensorFlow 程序包。
 
-# In[12]:
+# In[10]:
 
 
 def fully_conn(x_tensor, num_outputs):
@@ -350,9 +351,10 @@ def fully_conn(x_tensor, num_outputs):
     """
     # TODO: Implement Function
 
-    weights = tf.Variable(tf.truncated_normal([x_tensor.get_shape().as_list()[1], num_outputs]))
+    weights = tf.Variable(tf.truncated_normal([x_tensor.get_shape().as_list()[1], num_outputs], stddev = 0.05))
     biases = tf.Variable(tf.zeros(num_outputs))
-    return tf.add(tf.matmul(x_tensor, weights), biases)
+    fully_conn_layer = tf.add(tf.matmul(x_tensor, weights), biases)
+    return tf.nn.relu(fully_conn_layer)
 
 
 """
@@ -367,7 +369,7 @@ tests.test_fully_conn(fully_conn)
 # 
 # **注意**：该层级不应应用 Activation、softmax 或交叉熵（cross entropy）。
 
-# In[13]:
+# In[11]:
 
 
 def output(x_tensor, num_outputs):
@@ -379,7 +381,7 @@ def output(x_tensor, num_outputs):
     """
     # TODO: Implement Function
 
-    weights = tf.Variable(tf.truncated_normal([x_tensor.get_shape().as_list()[1], num_outputs]))
+    weights = tf.Variable(tf.truncated_normal([x_tensor.get_shape().as_list()[1], num_outputs], stddev = 0.05))
     biases = tf.Variable(tf.zeros(num_outputs))
     return tf.add(tf.matmul(x_tensor, weights), biases)
 
@@ -401,7 +403,7 @@ tests.test_output(output)
 # * 返回输出
 # * 使用 `keep_prob` 向模型中的一个或多个层应用 [TensorFlow 的 Dropout](https://www.tensorflow.org/api_docs/python/tf/nn/dropout)
 
-# In[40]:
+# In[49]:
 
 
 def conv_net(x, keep_prob):
@@ -415,37 +417,37 @@ def conv_net(x, keep_prob):
     #    Play around with different number of outputs, kernel size and stride
     # Function Definition from Above:
     #    conv2d_maxpool(x_tensor, conv_num_outputs, conv_ksize, conv_strides, pool_ksize, pool_strides)
-    conv_num_outputs = 64
+    #conv_num_outputs = 64
     num_outputs = 10
-    conv_ksize = (3, 3)
-    conv_strides = (2, 2)
-    pool_ksize = (3, 3)
-    pool_strides = (2, 2)
+    conv_ksize = (2, 2)
+    conv_strides = (1, 1)
+    pool_ksize = (2, 2)
+    pool_strides = (1, 1)
     
     #from time import time
     #t0 = time()
     #convolution and max pool layer 1
-    convolution_maxpool_layer = conv2d_maxpool(x, conv_num_outputs, 
-                                  conv_ksize, conv_strides, 
-                                 pool_ksize, pool_strides)
+    convolution_maxpool_layer = conv2d_maxpool(x, 64, 
+                                               conv_ksize, conv_strides, 
+                                               pool_ksize, pool_strides)
 
-    convolution_maxpool_layer = tf.nn.dropout(convolution_maxpool_layer, keep_prob)
+    
     #print("conv layer 1: {}s".format(time()-t0))
     
     #convolution and max pool layer 2
-    convolution_maxpool_layer = conv2d_maxpool(x, conv_num_outputs * 4, 
-                                  conv_ksize, conv_strides, 
-                                 pool_ksize, pool_strides)
+    #convolution_maxpool_layer = conv2d_maxpool(x, 128,conv_ksize, 
+    #                                           conv_strides,pool_ksize, 
+    #                                           pool_strides)
     
-    convolution_maxpool_layer = tf.nn.dropout(convolution_maxpool_layer, keep_prob)
+    
     #print("conv layer 2: {}s".format(time()-t0))
     
     #convolution and max pool layer 3
-    convolution_maxpool_layer = conv2d_maxpool(x, conv_num_outputs * 16, 
-                                  conv_ksize, conv_strides, 
-                                 pool_ksize, pool_strides)
+    #convolution_maxpool_layer = conv2d_maxpool(x, 256, 
+    #                                           conv_ksize, conv_strides,
+    #                                           pool_ksize, pool_strides)
 
-    convolution_maxpool_layer = tf.nn.dropout(convolution_maxpool_layer, keep_prob)
+    
     #print("conv layer 3: {}s".format(time()-t0))
     
     # TODO: Apply a Flatten Layer
@@ -460,25 +462,30 @@ def conv_net(x, keep_prob):
     #   fully_conn(x_tensor, num_outputs)
     
     # fully connected layer 1
-    fully_connected_layer = fully_conn(flatten_layer, conv_num_outputs * 16)
+    fully_connected_layer = fully_conn(flatten_layer, 64)
+    #fully_connected_layer = tf.nn.dropout(fully_connected_layer, keep_prob)
     #print("fully connected layer 1: {}s".format(time()-t0))
     
+    
     # fully connected layer 2
-    fully_connected_layer = fully_conn(fully_connected_layer, conv_num_outputs * 2)
+    #fully_connected_layer = fully_conn(fully_connected_layer, 256)
+    #fully_connected_layer = tf.nn.dropout(fully_connected_layer, keep_prob)
     #print("fully connected layer 2: {}s".format(time()-t0))
     
+    
     # fully connected layer 3
-    fully_connected_layer = fully_conn(fully_connected_layer, num_outputs)
+    #fully_connected_layer = fully_conn(fully_connected_layer, 64)
+    #fully_connected_layer = tf.nn.dropout(fully_connected_layer, keep_prob)
     #print("fully connected layer 3: {}s".format(time()-t0))
     
     # TODO: Apply an Output Layer
     #    Set this to the number of classes
     # Function Definition from Above:
     #   output(x_tensor, num_outputs)
-    output_layer = output(fully_connected_layer, num_outputs)
+    
     
     # TODO: return output
-    return output_layer
+    return output(fully_connected_layer, num_outputs)
 
 
 """
@@ -529,7 +536,7 @@ tests.test_conv_net(conv_net)
 # 注意：不需要返回任何内容。该函数只是用来优化神经网络。
 # 
 
-# In[34]:
+# In[13]:
 
 
 def train_neural_network(session, optimizer, keep_probability, feature_batch, label_batch):
@@ -542,10 +549,7 @@ def train_neural_network(session, optimizer, keep_probability, feature_batch, la
     : label_batch: Batch of Numpy label data
     """
     # TODO: Implement Function
-    session.run(optimizer, feed_dict = {
-        x:feature_batch, 
-        y:label_batch,
-        keep_prob:keep_probability})
+    session.run(optimizer, feed_dict = {x:feature_batch, y:label_batch, keep_prob:keep_probability})
     
 """
 DON'T MODIFY ANYTHING IN THIS CELL THAT IS BELOW THIS LINE
@@ -558,7 +562,7 @@ tests.test_train_nn(train_neural_network)
 # 实现函数 `print_stats` 以输出损失和验证准确率。使用全局变量 `valid_features` 和 `valid_labels` 计算验证准确率。使用保留率 `1.0` 计算损失和验证准确率（loss and validation accuracy）。
 # 
 
-# In[35]:
+# In[47]:
 
 
 def print_stats(session, feature_batch, label_batch, cost, accuracy):
@@ -571,9 +575,9 @@ def print_stats(session, feature_batch, label_batch, cost, accuracy):
     : accuracy: TensorFlow accuracy function
     """
     # TODO: Implement Function
-    accuracy = session.run(accuracy, feed_dict={x: feature_batch, y: label_batch, keep_prob: 1.0 })
-    loss = session.run(cost, feed_dict={x: feature_batch, y: label_batch, keep_prob: 1.0 })
-    print ("Testing Accuracy:{:.3f} and loss: {:.3f}".format(accuracy, loss) )
+
+    print ("Testing Accuracy:{:.3f} and loss: {:.3f}".format(session.run(accuracy, feed_dict={x: valid_features, y: valid_labels, keep_prob: 1.0 }),
+                                                             session.run(cost, feed_dict={x: feature_batch, y: label_batch, keep_prob: 1.0 })) )
 
 
 # ### 超参数
@@ -588,12 +592,12 @@ def print_stats(session, feature_batch, label_batch, cost, accuracy):
 #  * ...
 # * 设置 `keep_probability` 表示使用丢弃时保留节点的概率
 
-# In[36]:
+# In[43]:
 
 
 # TODO: Tune Parameters
 epochs = 10
-batch_size = 256
+batch_size = 32
 keep_probability = 0.5
 
 
@@ -602,7 +606,7 @@ keep_probability = 0.5
 # 我们先用单个部分，而不是用所有的 CIFAR-10 批次训练神经网络。这样可以节省时间，并对模型进行迭代，以提高准确率。最终验证准确率达到 50% 或以上之后，在下一部分对所有数据运行模型。
 # 
 
-# In[41]:
+# In[50]:
 
 
 """
@@ -626,7 +630,7 @@ with tf.Session() as sess:
 # 
 # 现在，单个 CIFAR-10 部分的准确率已经不错了，试试所有五个部分吧。
 
-# In[42]:
+# In[52]:
 
 
 """
@@ -662,7 +666,7 @@ with tf.Session() as sess:
 # 
 # 利用测试数据集测试你的模型。这将是最终的准确率。你的准确率应该高于 50%。如果没达到，请继续调整模型结构和参数。
 
-# In[43]:
+# In[53]:
 
 
 """
